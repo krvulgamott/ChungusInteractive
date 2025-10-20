e829df92541bb75d732b3e1d3671576297ce5f68
krvulgamott <krvulgamott@email.peace.edu>
2025-10-19 23:34:47 -0400

    Continued work for alpha
    Character work
    VFX and SFX integration
    Completed first (5+) builds

---
+++ SandsOfSet/Intermediate/Source/SandsOfSet.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class SandsOfSet : ModuleRules
+{
+	public SandsOfSet(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
